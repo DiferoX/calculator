df9b6aa9dc0b231233ae7ea3ed08b4b6cba49534
Diego <diferox@gmail.com>
2022-06-20 19:12:43 -0500

    add keyboard event

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function operate(num1, num2, simb)
   }
 }
 
-function prueba (n1, s)
+function firstValue (n1, s)
 {
   firstNum = Number(n1);
   simbol = s;
@@ -68,3 +68,72 @@ function reset()
   document.calculator.answ.value = "";
   document.calculator.show.value = "";
 }
+
+/*
+function probando(a)
+{
+  console.log(a);
+}
+*/
+
+function keyboard()
+{
+  key = event.keyCode;
+
+  //probando(key);
+
+  switch(key)
+  {
+    // Number Key
+    case 48:
+      return document.calculator.answ.value += '0';
+    case 49:
+      return document.calculator.answ.value += '1';
+    case 50:
+      return document.calculator.answ.value += '2';
+    case 51:
+      return document.calculator.answ.value += '3';
+    case 52:
+      return document.calculator.answ.value += '4';
+    case 53:
+      return document.calculator.answ.value += '5';
+    case 54:
+      return document.calculator.answ.value += '6';
+    case 55:
+      return document.calculator.answ.value += '7';
+    case 56:
+      return document.calculator.answ.value += '8';
+    case 57:
+      return document.calculator.answ.value += '9';
+    
+    
+    // Simbol Key
+    case 107:
+    case 187:
+      return firstValue(document.calculator.answ.value, '+');
+    case 109:
+    case 189:
+      return firstValue(document.calculator.answ.value, '-');
+    case 106:
+      return firstValue(document.calculator.answ.value, '*');
+    case 191:
+      return firstValue(document.calculator.answ.value, '/');
+    case 190:
+      return firstValue(document.calculator.answ.value, '.');
+
+    // Equal key
+    case 61:
+    case 13:
+      return equal(document.calculator.answ.value);
+
+    // Reset
+    case 27:  // Esc
+      return reset();
+    case 8:
+      return document.calculator.answ.value = '';
+
+  }
+  
+}
+
+window.onkeydown = keyboard;
